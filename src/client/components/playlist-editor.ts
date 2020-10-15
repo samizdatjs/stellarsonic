65a83cacc5ab2ef789f546ff4d09d82a2bbe973a
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-10-15 14:08:17 +0200

    feat(editing): simple drag drop for tracks

--- src/client/components/playlist-editor.ts
+++ src/client/components/playlist-editor.ts
@@ -1,9 +1,11 @@
-import {bindable} from 'aurelia-framework';
+import {bindable, bindingMode} from 'aurelia-framework';
 
 export class PlaylistEditorCustomElement {
-  @bindable data!: any;
+  @bindable({defaultBindingMode: bindingMode.twoWay}) data!: any;
 
   private selectedTrack: number = 0;
+  private dragSource: number | undefined;
+  private dragTarget: number | undefined;
 
   get track(): any {
     return this.data.tracks[this.selectedTrack];
@@ -11,5 +13,25 @@ export class PlaylistEditorCustomElement {
 
   selectTrack(index: number) {
     this.selectedTrack = index;
+  }
+
+  dragStart(event: DragEvent, index: number) {
+    this.dragSource = index;
+    return true;
+  }
+
+  dragOver(event: DragEvent, index: number) {
+    this.dragTarget = index;
+    return true;
+  }
+
+  dragDrop() {
+    if (this.dragSource !== undefined && this.dragTarget !== undefined) {
+      const target = this.data.tracks[this.dragTarget];
+      const source = this.data.tracks[this.dragSource];
+      this.data.tracks.splice(this.dragTarget, 1, source);
+      this.data.tracks.splice(this.dragSource, 1, target);
+    }
+    return true;
   }
 }
