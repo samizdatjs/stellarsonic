c51b7afaa32613d7da6304bbad90ef2a6bb8ed56
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-10-22 22:57:29 +0200

    correct schema.org type for track

--- src/domain/models/track.ts
+++ src/domain/models/track.ts
@@ -14,6 +14,7 @@ export class Track {
 
   public toJSONLD(): any {
     return {
+      '@type': 'MusicRecording',
       name: this.name,
       byArtist: this.byArtist,
       copyrightYear: this.copyrightYear,
