7bc5fcee77c73b06a0a7d448df23d2005996591a
Björn Andersson <bjorn.andersson2@cevt.se>
2020-11-02 16:59:10 +0100

    feat(home): initial work on editor and theme support in home

--- src/client/pages/home.ts
+++ src/client/pages/home.ts
@@ -1,28 +1,21 @@
 import {PostFeed, PostGenres} from '../main';
-import {autoinject, observable} from 'aurelia-framework';
-import { MusicPlaylist } from '../../domain/models/music-playlist';
+import {Editor} from '../services/editor';
+import {autoinject} from 'aurelia-framework';
 
 @autoinject
 export class Home {
-  @observable cols!: number;
-
   public constructor(
-    private posts: PostFeed,
-    private genres: PostGenres
+    private postFeed: PostFeed,
+    private genres: PostGenres,
+    private editor: Editor,
   ) {}
 
   async activate() {
-    this.posts.refresh();
-    this.genres.refresh();
+    await this.postFeed.refresh();
+    await this.genres.refresh();
   }
 
-  colsChanged(cols: number) {
-    this.posts.limit = cols > 3 ? cols : 6;
-    this.posts.increment = this.cols > 2 ? this.cols * 2 : 6;
-    this.posts.refresh();
-  }
-
-  image(post: MusicPlaylist): string {
-    return `/images/${encodeURIComponent(post.image)}`;
+  get posts() {
+    return this.postFeed.data;
   }
 }
