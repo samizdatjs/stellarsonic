3cee8010bcfbe21f6670268a089f8a6c2f213e2c
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-11-23 10:39:21 +0100

    fix(assets): load post assets properly

--- src/client/components/editor/panels/assets.ts
+++ src/client/components/editor/panels/assets.ts
@@ -1,11 +1,15 @@
 import {autoinject, PLATFORM} from 'aurelia-framework';
 import {ContentService} from '@client/services/content';
-import {EditorPanel} from '@client/interfaces';
+import {EditorPanel, Page} from '@client/interfaces';
 
 export class AssetsPanel extends EditorPanel {
   component = {
     viewModel: AssetsCustomElement,
     view: PLATFORM.moduleName('components/editor/panels/assets.html'),
+  }
+
+  constructor() {
+    super((page: Page) => page);
   }
 }
 
@@ -18,5 +22,12 @@ export class AssetsCustomElement {
   public constructor() {
     this.images = new ContentService('image', '')
     this.audio = new ContentService('audio', '')
+  }
+
+  activate(page: Page) {
+    if (page.content) {
+      this.images = new ContentService('image', `/images/${page.content._id}`);
+      this.audio = new ContentService('audio', `/audio/${page.content._id}`);
+    }
   }
 }
