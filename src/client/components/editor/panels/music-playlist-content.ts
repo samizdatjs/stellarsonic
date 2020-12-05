cf90e4c42c8e5b6519a92de7b0fe2486669252e4
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-12-05 23:08:00 +0100

    feat(editor): select input for audio

--- src/client/components/editor/panels/music-playlist-content.ts
+++ src/client/components/editor/panels/music-playlist-content.ts
@@ -1,11 +1,15 @@
 import {autoinject, PLATFORM} from 'aurelia-framework';
 import {ContentEditorComponent, EditorComponentConfig} from '@client/interfaces';
 import {Editor} from '@client/services/editor';
+import { Assets } from '@client/services/assets';
 
 @autoinject
 export class MusicPlaylistContentCustomElement extends ContentEditorComponent {
+  audio: Assets;
+
   public constructor(editor: Editor) {
     super(editor);
+    this.audio = editor.page.audio;
   }
 }
 
