2e140463c5ec4df8a146f61b6c7177c188b16fc6
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-12-05 21:01:54 +0100

    feat(editor): working post creation

--- src/client/components/editor/panels/post-list.ts
+++ src/client/components/editor/panels/post-list.ts
@@ -1,21 +1,32 @@
 import {inject, PLATFORM} from 'aurelia-framework';
 import {MusicPlaylist} from '@domain/models/music-playlist';
-import {PostListView} from '@client/views';
+import {PostListView, AuthorListView} from '@client/views';
 import {action, EditorComponent, EditorComponentConfig} from '@client/interfaces';
+import UIkit from 'uikit';
 
-@inject(PostListView)
+@inject(PostListView, AuthorListView)
 export class PostListCustomElement extends EditorComponent {
   public selected: MusicPlaylist | undefined;
+  author: any;
 
-  constructor(public posts: PostListView) { super(); }
+  constructor(public posts: PostListView, public authors: AuthorListView) { super(); }
 
   async bind() {
     this.posts.refresh();
   }
 
   @action({title: 'create', icon: 'plus'})
-  createPost() {
+  async createPost() {
+    await this.authors.refresh();
     this.selected = new MusicPlaylist();
+    UIkit.modal('#post-create-modal').show();
+  }
+
+  async save() {
+    if (this.selected) {
+      this.selected.author = this.author;
+      this.posts.save(this.selected);
+    }
   }
 }
 
