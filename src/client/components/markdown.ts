21e4a4eb8aa2ac47d93d91e5830c6202c8affed4
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-12-05 21:42:32 +0100

    feat(markdown): embedded youtube support in markdown

--- src/client/components/markdown.ts
+++ src/client/components/markdown.ts
@@ -1,9 +1,10 @@
 import {bindable} from 'aurelia-framework';
 import * as showdown from 'showdown';
+require('showdown-youtube');
 
 export class MarkdownCustomElement {
   @bindable text!: string;
-  converter = new showdown.Converter();
+  converter = new showdown.Converter({extensions: ['youtube']});
 
   get html(): string {
     return this.converter.makeHtml(this.text);
