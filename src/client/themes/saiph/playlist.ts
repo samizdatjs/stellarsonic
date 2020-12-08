9a008c1b61d9c6ed3fbc51d00135944955d90add
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-12-08 21:00:55 +0100

    feat(saiph): radial gradient background

--- src/client/themes/saiph/playlist.ts
+++ src/client/themes/saiph/playlist.ts
@@ -3,16 +3,22 @@ import {Player} from '@domain/player';
 import {MusicPlaylist} from '@domain/models/music-playlist';
 import {SaiphPlaylistTheme} from '.';
 import {PageView} from '@client/interfaces';
+import {Color} from '@client/services/color';
 
 @autoinject
 export class Playlist extends PageView<MusicPlaylist, SaiphPlaylistTheme> {
   public constructor(
     public player: Player,
+    public color: Color,
   ) { super() }
 
   get icon(): string {
     return !this.player.isLoaded(this.content) || this.player.audio.paused
       ? 'fal fa-play-circle'
       : 'fal fa-pause-circle';
+  }
+
+  get background(): string {
+    return `radial-gradient(${this.color.lighten(this.theme.backgroundColor, 20)}, ${this.theme.backgroundColor})`
   }
 }
