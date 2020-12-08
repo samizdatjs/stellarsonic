37228b06ad062d22f873f9c6927e76236f7efe5c
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-12-08 23:51:36 +0100

    feat(saiph): clone home from standard theme

--- src/client/themes/saiph/home.ts
+++ src/client/themes/saiph/home.ts
@@ -3,22 +3,18 @@ import {PostFeed} from '@client/views';
 import {RouteConfig} from 'aurelia-router';
 
 @autoinject
-export class SaiphHome {
+export class Home {
   settings: any;
-  // bottom!: HTMLElement;
+  bottom!: HTMLElement;
 
-  constructor(public postFeed: PostFeed) {
-    console.log('saiph home constructor');
-  }
+  constructor(public postFeed: PostFeed) {}
 
   async activate(params: any, routeConfig: RouteConfig) {
-    console.log('activate saiph');
-    // this.postFeed.limit = 6;
-    // await this.postFeed.refresh();
+    this.postFeed.limit = 6;
+    await this.postFeed.refresh();
   }
-/*
+
   get posts() {
     return this.postFeed.data;
   }
-  */
 }
