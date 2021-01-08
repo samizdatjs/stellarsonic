2820aaea1018e940ceebecde089a01b2ba4d2292
Björn Andersson <bjorn.olof.andersson@gmail.com>
2021-01-08 20:13:55 +0100

    style(editor): 2 spaces indentation in editor config

--- src/client/editorConfig.ts
+++ src/client/editorConfig.ts
@@ -13,20 +13,20 @@ import {image} from './components/editor/panels/image';
 import {MenuItem} from './interfaces';
 
 export const editorConfig: MenuItem[] = [
-    { title: 'Site' },
-    { title: 'Settings', icon: 'cog', component: siteSettings },
-    { title: 'Posts', icon: 'file-edit', component: postList },
-    { title: 'Authors', icon: 'users', component: authorList },
-    { title: 'Page' },
-    { title: 'Settings', icon: 'cog', component: pageSettings },
-    { title: 'Palette', icon: 'paint-bucket', component: pagePalette },
-    { title: 'Template', icon: 'settings', component: theme },
-    { title: 'Assets', icon: 'cloud-upload', component: assets },
-    { type: 'playlist', title: 'Content' },
-    { type: 'playlist', title: 'General', icon: 'file-edit', component: playlistContent },
-    { type: 'playlist', title: 'Cover', icon: 'image', component: image('image') },
-    { type: 'playlist', title: 'Tags', icon: 'tag', component: tagList('keywords') },
-    { type: 'playlist', title: 'Genres', icon: 'tag', component: tagList('genres') },
-    { type: 'playlist', title: 'Text', icon: 'file-text', component: textEditor('text') },
-    { type: 'playlist', title: 'Playlist', icon: 'play', component: playlistTracks }
+  { title: 'Site' },
+  { title: 'Settings', icon: 'cog', component: siteSettings },
+  { title: 'Posts', icon: 'file-edit', component: postList },
+  { title: 'Authors', icon: 'users', component: authorList },
+  { title: 'Page' },
+  { title: 'Settings', icon: 'cog', component: pageSettings },
+  { title: 'Palette', icon: 'paint-bucket', component: pagePalette },
+  { title: 'Template', icon: 'settings', component: theme },
+  { title: 'Assets', icon: 'cloud-upload', component: assets },
+  { type: 'playlist', title: 'Content' },
+  { type: 'playlist', title: 'General', icon: 'file-edit', component: playlistContent },
+  { type: 'playlist', title: 'Cover', icon: 'image', component: image('image') },
+  { type: 'playlist', title: 'Tags', icon: 'tag', component: tagList('keywords') },
+  { type: 'playlist', title: 'Genres', icon: 'tag', component: tagList('genres') },
+  { type: 'playlist', title: 'Text', icon: 'file-text', component: textEditor('text') },
+  { type: 'playlist', title: 'Playlist', icon: 'play', component: playlistTracks }
 ]
